87d5a97aa4e2663ab68e82a54fedd82bc2cd516a
Your Name <abderrahmanerrachidygmail.com>
2025-03-07 10:31:23 +0000

    add test file

---
+++ src/test/java/org/exemple/MainTest.java
@@ -0,0 +1,16 @@
+package org.exemple;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+    @Test
+    public void testMain() {
+        // Ceci est un test très basique.  Pour un vrai test, vous devriez
+        // vérifier quelque chose de significatif dans votre application.
+        // Dans cet exemple, on vérifie simplement que 1 + 1 est égal à 2.
+
+        assertEquals(2, 1 + 1);
+    }
+}
