6b92a52dc4c1cde91e94dd5d201ce4fbc8a02e9e
Abderrahman ER-RACHIDI <94928114+errachidy10@users.noreply.github.com>
2025-04-05 23:04:43 +0000

    Update MainTest.java

--- src/test/java/org/exemple/MainTest.java
+++ src/test/java/org/exemple/MainTest.java
@@ -11,6 +11,6 @@ public class MainTest {
         // vérifier quelque chose de significatif dans votre application.
         // Dans cet exemple, on vérifie simplement que 1 + 1 est égal à 2.
 
-        assertEquals(44, 1 + 10);
+        assertEquals(22, 11 + 11);
     }
 }
